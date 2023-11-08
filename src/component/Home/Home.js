02a833094f87b2510bc54b7f184f8aef3c4064ff
Mayank00480 <mayanksharma1937@gmail.com>
2023-11-09 01:13:49 +0530

    Verify Email

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -4,6 +4,24 @@ import { Link } from 'react-router-dom'
 import './Home.css'
 const Home = () => {
     const context = useContext(ContextStore)
+    const verifyEmail = () =>{
+      fetch('https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyDTR6ElU-dwM_da4ZXS4s7leT8d6kyUaI4',{
+        method : 'POST',
+        body : JSON.stringify({
+          requestType : "VERIFY_EMAIL",
+          idToken : localStorage.getItem("expenseToken") 
+        })
+      })
+      .then(res => res.json())
+      .then(resp =>{
+        if(resp.error){
+          alert(resp.error.message)
+        }
+        else{
+          console.log(resp);
+        }
+      })
+    }
   return (
     <>
      <button onClick = {() => {context.removeToken()
@@ -11,6 +29,7 @@ const Home = () => {
      }}> Logout</button>
      <div className = "header" >
       <p >Welcome to Expense Tracker</p>
+      <button onClick = {verifyEmail}> verifyEmailId</button>
       <Link className = "link" to = "/profile" >Your Profile is incomplete</Link>
       </div>
     </>
